455ad665de18e36fe98d71f91fbc6a615269ebf8
eablak <esrablk9@gmail.com>
2026-04-29 08:34:33 +0300

    update train

--- 04_classification/train.py
+++ 04_classification/train.py
@@ -8,10 +8,11 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from tensorflow.keras.regularizers import L2
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-
+from tensorflow.keras.callbacks import EarlyStopping
 
 def get_images(directory):
     
@@ -19,7 +20,7 @@ def get_images(directory):
     
     X = []
     y = []
-    img_size = (100, 100)
+    img_size = (128, 128)
     
     for folder in os.listdir(path):
         path_folder = str(path) + "/" + folder
@@ -58,36 +59,45 @@ def split_dataset(X, y):
     return X_train, X_valid, y_train, y_valid
 
 
-def train_model(X_valid, y_valid):
+def train_model(X_train, X_valid, y_train, y_valid):
 
     model = Sequential([
-        Conv2D(32, (3,3), activation = 'relu', input_shape = (100, 100, 3)),
+        Conv2D(32, (3,3), activation = 'relu', input_shape = (128, 128, 3)),
         MaxPooling2D((2,2)),
 
-        Conv2D(32, (3,3), activation = 'relu'),
+
+        Conv2D(64, (3,3), activation = 'relu'),
         MaxPooling2D((2,2)),
+        Dropout(0.15),
+
+
+        Conv2D(128, (3,3), activation = 'relu'),
+        MaxPooling2D((2,2)),
+
 
         Flatten(),
-        Dense(64, activation = 'relu'),
-        Dense(4, activation = 'softmax')])
+        Dense(128, activation = 'relu', kernel_regularizer=L2(0.0001)),
+        Dropout(0.35),
+        Dense(4, activation = 'softmax')
+    ])
     
 
     model.compile(loss = 'sparse_categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
-    model.fit(X_train, y_train, epochs = 5, batch_size = 64)
+
+    callbacks = [EarlyStopping(monitor='val_accuracy', patience=3, restore_best_weights=True, mode='max')]
+
+    model.fit(X_train, y_train, epochs = 20, batch_size = 64, validation_data=(X_valid, y_valid), callbacks=callbacks)
 
     model.evaluate(X_valid, y_valid)
 
     return model
 
 
-def pred_img(model, X_valid, y_valid):
-    
-    idx2 = random.randint(0, len(y_valid))
-    # plt.imshow(X_valid[idx2, :])
-    # plt.show()
+def save_model(model, directory):
 
-    y_pred = model.predict(X_valid[idx2, :].reshape(1, 100, 100, 3))
-    print(y_pred)
+    file_name = directory + "_model.h5"
+    model.save(file_name, include_optimizer=True)
+    print("Model saved!")
 
 
 
@@ -101,23 +111,8 @@ if __name__ == "__main__":
 
     X, y = get_images(directory)
     X_train, X_valid, y_train, y_valid = split_dataset(X, y)
-    model = train_model(X_valid, y_valid)
-    # save_model(model)
-    pred_img(model, X_valid, y_valid)
+    
+    model = train_model(X_train, X_valid, y_train, y_valid)
+    model.summary()
 
-
-    # Example results
-
-    # Epoch 1/5
-    # 163/163 ━━━━━━━━━━━━━━━━━━━━ 10s 59ms/step - accuracy: 0.5477 - loss: 1.0922  
-    # Epoch 2/5
-    # 163/163 ━━━━━━━━━━━━━━━━━━━━ 10s 61ms/step - accuracy: 0.7628 - loss: 0.6409 
-    # Epoch 3/5
-    # 163/163 ━━━━━━━━━━━━━━━━━━━━ 10s 62ms/step - accuracy: 0.8396 - loss: 0.4362 
-    # Epoch 4/5
-    # 163/163 ━━━━━━━━━━━━━━━━━━━━ 11s 68ms/step - accuracy: 0.8734 - loss: 0.3503 
-    # Epoch 5/5
-    # 163/163 ━━━━━━━━━━━━━━━━━━━━ 11s 69ms/step - accuracy: 0.9064 - loss: 0.2641 
-    # 82/82 ━━━━━━━━━━━━━━━━━━━━ 1s 16ms/step - accuracy: 0.7470 - loss: 0.6916
-    # [[3.0113186e-03 7.6050006e-02 1.1677919e-07 9.2093861e-01]]
-
+    save_model(model, directory)
