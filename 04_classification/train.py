9251e28ae5bffd81893dbd687184123d680cd38d
eablak <esrablk9@gmail.com>
2026-05-02 21:59:06 +0300

    start new architecture

--- 04_classification/train.py
+++ 04_classification/train.py
@@ -1,118 +1,95 @@
-import cv2
-import numpy as np
-import random
-import argparse
-from pathlib import Path
 import os
-import matplotlib
-matplotlib.use('Agg')
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from sklearn.utils.class_weight import compute_class_weight
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, GlobalAveragePooling2D, Dropout, Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+import seaborn as sns
 import matplotlib.pyplot as plt
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
-from tensorflow.keras.regularizers import L2
+from sklearn.metrics import confusion_matrix
+import cv2
+from sklearn.preprocessing import LabelEncoder , OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras import layers
+from matplotlib.pyplot import imread
+from matplotlib.pyplot import imshow
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.imagenet_utils import decode_predictions
+from tensorflow.keras.applications.imagenet_utils import preprocess_input
+from pathlib import Path
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-def get_images(directory):
+dataset_path = Path(os.getcwd()).parent / "transformed_images"
+
+
+def get_class_infos():
+
+    print("Dataset classes are:", [x for x in os.listdir(dataset_path)])
+    print("\nTypes of classes labels:", len(os.listdir(dataset_path)))
+    print("\n", "*"*60, "\n")
+
+
+def convert_images_to_df():
     
-    path = Path(os.getcwd()).parent / "transformed_images" / directory
+    data_list = []
+
+    for root, dirs, files in os.walk(dataset_path):
+        class_name = os.path.basename(root)
+        if class_name == "transformed_images":
+            continue
+        for file in files:
+            data_list.append([class_name, os.path.join(root, file)])
+
+    df = pd.DataFrame(data=data_list, columns=['Labels', 'image'])
+
+    print(df.head())
+    print(df.tail())
+    print("\n")
+
+    label_count = df['Labels'].value_counts()
+    print(label_count)
+    print("\n","*"*60)
+
+
+def resize_images():
+
+    im_size = 224
+
+    images = []
+    labels = []
+
+    for data_path in dataset_path.iterdir():
+        filenames = [i for i in os.listdir(data_path) ]
     
-    X = []
-    y = []
-    img_size = (128, 128)
+        for f in filenames:
+            img = cv2.imread(str(data_path) + '/' + f)
+            img = cv2.resize(img, (im_size, im_size))
+            images.append(img)
+            labels.append(data_path.name)
+
+    datagen = ImageDataGenerator(rescale = 1./255)
     
-    for folder in os.listdir(path):
-        path_folder = str(path) + "/" + folder
+    train_generator = datagen.flow_from_directory(str(dataset_path), target_size=(224, 224), batch_size=32)
+    images_batch, labels_batch = next(train_generator)
 
-        for img_name in os.listdir(Path(path_folder)):
-
-            img = cv2.imread(path_folder + "/" + img_name)
-            img = cv2.resize(img, img_size)
-            # print(img.shape)
-            X.append(img)
-            y.append(folder)
-
-    
-    X = np.array(X)
-    y = np.array(y)
-
-    X = X / 255.0
-    y = y.reshape(len(y), 1)
-    y = LabelEncoder().fit_transform(y.ravel())
-
-    # print(X.shape)
-    # print(y.shape)
-
-    return X, y
-
-
-def split_dataset(X, y):
-    
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y,test_size=0.2,train_size=0.8)
-    
-    # print(X_train.shape)
-    # print(X_valid.shape)
-    # print(y_train.shape)
-    # print(y_train.shape)
-
-    return X_train, X_valid, y_train, y_valid
-
-
-def train_model(X_train, X_valid, y_train, y_valid):
-
-    model = Sequential([
-        Conv2D(32, (3,3), activation = 'relu', input_shape = (128, 128, 3)),
-        MaxPooling2D((2,2)),
-
-
-        Conv2D(64, (3,3), activation = 'relu'),
-        MaxPooling2D((2,2)),
-        Dropout(0.15),
-
-
-        Conv2D(128, (3,3), activation = 'relu'),
-        MaxPooling2D((2,2)),
-
-
-        Flatten(),
-        Dense(128, activation = 'relu', kernel_regularizer=L2(0.0001)),
-        Dropout(0.35),
-        Dense(4, activation = 'softmax')
-    ])
-    
-
-    model.compile(loss = 'sparse_categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
-
-    callbacks = [EarlyStopping(monitor='val_accuracy', patience=3, restore_best_weights=True, mode='max')]
-
-    model.fit(X_train, y_train, epochs = 20, batch_size = 64, validation_data=(X_valid, y_valid), callbacks=callbacks)
-
-    model.evaluate(X_valid, y_valid)
-
-    return model
-
-
-def save_model(model, directory):
-
-    file_name = directory + "_model.h5"
-    model.save(file_name, include_optimizer=True)
-    print("Model saved!")
-
+    # rn per batch. write it into func and call it in model training time
+    print(images_batch.shape)
+    print(labels_batch.shape)
 
 
 if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", help="directory")
-
-    args = parser.parse_args()
-    directory = args.d
-
-    X, y = get_images(directory)
-    X_train, X_valid, y_train, y_valid = split_dataset(X, y)
     
-    model = train_model(X_train, X_valid, y_train, y_valid)
-    model.summary()
+    # Step 1: Data Preprocessing
 
-    save_model(model, directory)
+    get_class_infos()
+    convert_images_to_df()
+    resize_images()
